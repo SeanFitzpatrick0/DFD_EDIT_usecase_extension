8e726601fdc45b191cf8aa97a17d0eb4bc95045d
Sean Fitzpatrick <seanlkfitzpatrick@gmail.com>
2020-03-28 21:06:47 +0000

    Adding the associated data propert to all occurances of a df or ds in the RDF

--- App/exporter.py
+++ App/exporter.py
@@ -17,13 +17,16 @@ def export_dfd(dfd):
 
 def create_dfd_rdf(dfd):
     # Collect all items in DFD
-    entities, processes, datastores, dataflows = collect_items(dfd)
+    dataflow_associations, datastore_associations = collect_item_associations(
+        dfd)
+    entities, processes, datastores, dataflows = collect_items(
+        dfd, dataflow_associations, datastore_associations)
     # Create RDF
     rdf_graph = create_rdf_graph(entities, processes, datastores, dataflows)
     return rdf_graph
 
 
-def collect_items(hierarchy):
+def collect_items(hierarchy, dataflow_associations, datastore_associations):
     # Set items records
     entities = set()    # Entity names
     processes = {}      # Process name (key) and parent name or None (value)
@@ -48,24 +51,56 @@ def collect_items(hierarchy):
     for datastore in graph.find_all('datastore'):
         if not datastore.get('from_parent'):
             label = datastore.get('label')
-            associated_data = datastore.find('mxcell').get('associated_data')
+            associated_data = datastore_associations[label] if label in datastore_associations else None
             datastores[label] = associated_data
 
     for flow in graph.find_all('mxcell', {"item_type": "flow"}):
         label = flow.get('value')
         source = graph.find(attrs={'id': flow.get('source')}).get('label')
         target = graph.find(attrs={'id': flow.get('target')}).get('label')
-        associated_data = flow.get('associated_data')
+        associated_data = dataflow_associations[label] if label in dataflow_associations else None
         dataflows.add((label, source, target, associated_data))
 
     # Collect items in sub processes and merge them
     for child in hierarchy['children']:
-        _, sub_processes, sub_datastores, sub_dataflows = collect_items(child)
+        _, sub_processes, sub_datastores, sub_dataflows = collect_items(
+            child, dataflow_associations, datastore_associations)
         processes.update(sub_processes)
         datastores.update(sub_datastores)
         dataflows.update(sub_dataflows)
 
     return entities, processes, datastores, dataflows
+
+
+def collect_item_associations(hierarchy):
+    # Data flow Name (Key) and Associated Data URI (Value)
+    dataflow_associations = {}
+    # Data Store Name (Key) and Associated Data URI (Value)
+    datastore_associations = {}
+
+    # Find associated data items in graph
+    graph = BeautifulSoup(hierarchy['xml_model'], 'lxml')
+
+    for flow in graph.find_all('mxcell', {"item_type": "flow"}):
+        label = flow.get('value')
+        if flow.get('associated_data'):
+            dataflow_associations[label] = flow.get('associated_data')
+
+    for datastore in graph.find_all('datastore'):
+        if not datastore.get('from_parent'):
+            label = datastore.get('label')
+            if datastore.find('mxcell').get('associated_data'):
+                datastore_associations[label] = datastore.find(
+                    'mxcell').get('associated_data')
+
+    # Collect associated data in sub processes and merge them
+    for child in hierarchy['children']:
+        sub_dataflow_associations, sub_datastore_associations = collect_item_associations(
+            child)
+        dataflow_associations.update(sub_dataflow_associations)
+        datastore_associations.update(sub_datastore_associations)
+
+    return dataflow_associations, datastore_associations
 
 
 def create_rdf_graph(entities, processes, datastores, dataflows):
